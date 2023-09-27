a686681faeed40bc8405efc22cf96446050679ee
qbobl5 <ddddd991125@gmail.com>
2023-09-27 16:15:11 +0900

    Create TaskInfo.java

---
+++ src/main/java/com/my/team/board/task/dto/TaskInfo.java
@@ -0,0 +1,17 @@
+package com.my.team.board.task.dto;
+
+import java.util.Date;
+
+public class TaskInfo {
+
+	Integer taskNo;
+	Integer teamMemberNo;
+	Date dueDate1;
+	Date dueDate2;
+	String title;
+	String type;
+	Date regDate;
+	String endDate;
+	Double avgReviewScore;
+	
+}
