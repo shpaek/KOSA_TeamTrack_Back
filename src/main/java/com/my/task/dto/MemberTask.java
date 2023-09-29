5269f7d86f049592fd8c180a76d2fea24e3a4be0
qbobl5 <ddddd991125@gmail.com>
2023-09-29 21:06:07 +0900

    [Feat] TaskDTO 수정

---
+++ src/main/java/com/my/task/dto/MemberTask.java
@@ -0,0 +1,20 @@
+package com.my.task.dto;
+
+import java.util.Date;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class MemberTask {
+
+	private Task task;
+	private int hwScore;
+	private int reviewScore;
+	private Date submitDate;
+	private List<Integer> memberAnswerList;
+	
+}
