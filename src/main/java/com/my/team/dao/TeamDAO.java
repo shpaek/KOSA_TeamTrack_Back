b37dfe42e3a14f11c6ec8e5ebc7fe1ce74bb262d
shpaek <seohpp@gmail.com>
2023-10-04 15:10:12 +0900

    [feat] Team 패키지 추가

---
+++ src/main/java/com/my/team/dao/TeamDAO.java
@@ -0,0 +1,81 @@
+package com.my.team.dao;
+
+import com.my.exception.AddException;
+import com.my.exception.FindException;
+import com.my.exception.ModifyException;
+import com.my.exception.RemoveException;
+import com.my.team.dto.Team;
+
+public interface TeamDAO {
+	/**
+	 * 서비스에 등록된 전체 팀을 조회한다
+	 * @return 팀객체
+	 */
+	Team selectAll();
+	
+	/**
+	 * 팀번호에 해당하는 팀정보를 검색한다
+	 * @param teamNo 팀번호
+	 * @return 팀객체
+	 * @throws FindException DB와의 연결실패 또는 제약조건위배일 경우 예외발생한다
+	 */
+	Team selectByTeamNo(int teamNo) throws FindException;
+	
+	/**
+	 * 팀이름에 해당하는 팀정보를 검색한다
+	 * @param teamName 팀이름
+	 * @return 팀객체
+	 * @throws FindException DB와의 연결실패 또는 제약조건위배일 경우 예외발생한다
+	 */
+	Team selectByTeamName(String teamName) throws FindException;
+	
+	/**
+	 * 해시태그에 해당하는 팀을 검색한다
+	 * @param hashtag 해시태그
+	 * @return 팀객체
+	 * @throws FindException
+	 */
+	Team selectByHashtag(String hashtag) throws FindException;
+	
+	/**
+	 * 최신순으로 팀을 조회한다
+	 * @return 팀객체
+	 */
+	Team selectByNewTeam();
+	
+	/**
+	 * 조회수순으로 팀을 조회한다
+	 * @return 팀객체
+	 */
+	Team selectByViewCnt();
+	
+	/**
+	 * 스터디날짜에 해당하는 팀을 조회한다
+	 * @param startDate 스터디 시작일자
+	 * @param endDate 스터디 종료일자
+	 * @return 팀객체
+	 */
+	Team selectByStudyDate(String startDate, String endDate);
+	
+	/**
+	 * 팀을 추가한다
+	 * @param c 팀객체
+	 * @throws AddException DB와의 연결실패 또는 제약조건위배일 경우 예외발생한다
+	 */
+	void create(Team t) throws AddException;
+	
+	/**
+	 * 팀정보를 수정한다
+	 * @param teamNo 팀번호
+	 * @throws ModifyException DB와의 연결실패 또는 제약조건위배일 경우 예외발생한다
+	 */
+	void update(int teamNo) throws ModifyException;
+	
+	/**
+	 * 팀을 삭제한다
+	 * @param teamNo 팀번호
+	 * @throws RemoveException DB와의 연결실패 또는 제약조건위배일 경우 예외발생한다
+	 */
+	void delete(int teamNo) throws RemoveException;
+	
+}
---
+++ src/main/java/control/TeamController.java
@@ -0,0 +1,12 @@
+package control;
+
+import com.my.team.service.TeamServiceImpl;
+
+public abstract class TeamController implements Controller {
+
+	protected TeamServiceImpl service;
+	
+	public TeamController() {
+		service = TeamServiceImpl.getInstance();
+	}
+}
