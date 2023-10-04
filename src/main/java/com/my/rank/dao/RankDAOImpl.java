c7f03ad97a2f0d1a4ea35671549c6a3af8b2fdf7
ykchoi7 <7dbsrud7@gmail.com>
2023-10-04 16:04:45 +0900

    [feat]RankDAO, rank.xml

--- src/main/java/com/my/rank/dao/RankDAOImpl.java
+++ src/main/java/com/my/rank/dao/RankDAOImpl.java
@@ -1,29 +1,66 @@
 package com.my.rank.dao;
 
+import java.io.InputStream;
 import java.lang.module.FindException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import com.my.exception.AddException;
 import com.my.rank.dto.RankDTO;
 
 public class RankDAOImpl implements RankDAO {
-
-
-	@Override
-	public List<RankDTO> selectByMonth(int teamNo, Date rankDate) throws FindException {
-		// TODO Auto-generated method stub
-		return null;
+	
+	private SqlSessionFactory sqlSessionFactory;
+	
+	public RankDAOImpl() {
+		String resource = "com/my/sql/mybatis-config.xml";
+		InputStream inputStream;
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
-	public void insert(int teamNo, RankDTO rankDto) throws AddException {
+	public List<RankDTO> selectByMonth(Integer teamNo, Date rankDate) throws FindException {
+		SqlSession session = null;
+		List<RankDTO> list = new ArrayList<>(); 
+		
+		try {
+			session = SqlSessionFactory.openSession();
+			Map<String, Object> map = new HashMap<>();
+			map.put("teamno", teamNo);
+			map.put("date", rankDate);
+			list = session.selectList("com.my.rank.RankMapper.selectByMonth", map);
+			return list;
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new FindException(e.getMessage());
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}
+	}
+
+	@Override
+	public void insert(Integer teamNo, RankDTO rankDto) throws AddException {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public List<AttendanceDTO> selectAttendanceDay(int teamNo, Date attendanceDate) throws FindException {
+	public List<AttendanceDTO> selectAttendanceDay(Integer teamNo, Date attendanceDate) throws FindException {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -35,27 +72,33 @@ public class RankDAOImpl implements RankDAO {
 	}
 
 	@Override
-	public List<TaskDTO> countMonthlyTask(int teamNo, Date endDate) throws FindException {
+	public List<TaskDTO> countMonthlyTask(Integer teamNo, Date endDate) throws FindException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public List<MemberScoreDTO> selectTaskScore(int teamNo, Date submitDate) throws FindException {
+	public List<MemberScoreDTO> selectTaskScore(Integer teamNo, Date submitDate) throws FindException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public List<TaskDTO> selectReviewScore(int teamNo, Date endDate) throws FindException {
+	public List<TaskDTO> selectReviewScore(Integer teamNo, Date endDate) throws FindException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public List<QnACommentDTO> selectQnAScore(int teamNo, Date pickedDate) throws FindException {
+	public List<QnACommentDTO> selectQnAScore(Integer teamNo, Date pickedDate) throws FindException {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public void selectAllTask(TaskDTO taskDto) throws FindException {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
