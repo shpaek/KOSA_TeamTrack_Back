c15fb77ede00a5f6e84376ce5fbad9d85ae95e17
오찬석 <dhcksehf1@naver.com>
2023-10-06 17:26:53 +0900

    [feat] QnaBoardDAO,Service 구현

--- src/main/java/com/my/qna/dto/QnaBoardDTO.java
+++ src/main/java/com/my/qna/dto/QnaBoardDTO.java
@@ -4,10 +4,12 @@ import java.util.Date;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class QnaBoardDTO {
 
