ea26b965da9829171aaec8ca6fedf16cb410b1d7
sengna <jeonseino_o@naver.com>
2023-10-09 16:49:23 +0900

    [Fix] 팀 메인 페이지 프론트 작업 중

--- src/main/java/control/DispatcherServlet.java
+++ src/main/java/control/DispatcherServlet.java
@@ -32,10 +32,13 @@ public class DispatcherServlet extends HttpServlet {
 		env = new Properties(); // Properties객체 생성
 		//servletContext 객체 찾기
 		ServletContext sc = this.getServletContext();
-		//String realPath = sc.getRealPath("WEB-INF\\classes\\com\\my\\env\\" + envFileName); //window
+		
+//		String realPath = sc.getRealPath("WEB-INF\\classes\\com\\my\\env\\" + envFileName); //window
 		String realPath = sc.getRealPath("WEB-INF/classes/com/my/env/" + envFileName); //mac
+		
 		System.out.println("in DispathcerServlet의 init:() realPath = " + realPath);
 		// control.properties파일의 내용을 Properties객체에 넣기
+		
 		try {
 			env.load(new FileInputStream(realPath));
 		} catch (IOException e) {
