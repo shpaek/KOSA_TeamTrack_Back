ef92681a38dea5c5efa4a80b86f5483b82cd9098
qbobl5 <ddddd991125@gmail.com>
2023-10-13 18:22:08 +0900

    [Fix] 팀 수정

--- src/main/java/com/my/team/dao/TeamDAOImpl.java
+++ src/main/java/com/my/team/dao/TeamDAOImpl.java
@@ -590,11 +590,8 @@ public class TeamDAOImpl implements TeamDAO {
 
 			int selectedTeamMemberStatus = session.selectOne("com.my.team.TeamMapper.selectTeamMemberStatus", map);
 
-			if(selectedTeamMemberStatus == 1) {
-				return selectedTeamMemberStatus;
-			} else {
-				throw new FindException("해당 팀의 팀원이 아닙니다.");
-			} // if-else
+			return selectedTeamMemberStatus;
+			
 		} catch(Exception e) {
 			throw new FindException(e.getMessage());
 		} finally {
--- src/main/java/control/TeamMainController.java
+++ src/main/java/control/TeamMainController.java
@@ -32,7 +32,10 @@ public class TeamMainController extends TeamController {
 		Map<String, Object> statusMap = new HashMap<>();
 		
         int teamNo = Integer.parseInt(request.getParameter("teamNo"));
-        String id = request.getParameter("id");
+        //String id = request.getParameter("id");
+//        String id="psh2023";
+//        String id="khb2023";
+        String id="cjs1231";
 
         try {
         	
