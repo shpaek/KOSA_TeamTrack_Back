d1fa1c0f95d97de22573723914794130040c70c8
sengna <jeonseino_o@naver.com>
2023-10-15 05:39:10 +0900

    [Feat] 세션 아이디 가져오기 성공

--- src/main/java/control/LoginController.java
+++ src/main/java/control/LoginController.java
@@ -16,62 +16,68 @@ import com.my.exception.FindException;
 
 public class LoginController extends CustomerController {
 
-	@Override
-	public String execute(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+   @Override
+   public String execute(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
-		// CORS 문제 해결
-		res.setHeader("Access-Control-Allow-Origin", "http://localhost:5500");
-//		res.setHeader("Access-Control-Allow-Origin", "http://192.168.1.105:5500");
-		res.setHeader("Access-Control-Allow-Credentials", "true");
+      // CORS 문제 해결
+      res.setHeader("Access-Control-Allow-Origin", "http://localhost:5500");
+//      res.setHeader("Access-Control-Allow-Origin", "http://localhost:5500");
+//      res.setHeader("Access-Control-Allow-Origin", "http://192.168.1.105:5500");
+      res.setHeader("Access-Control-Allow-Credentials", "true");
 
-		res.setContentType("application/json; charset=utf-8");
+      res.setContentType("application/json; charset=utf-8");
 
-		// 응답 출력스트림 얻기
-		PrintWriter out = res.getWriter();
+      // 응답 출력스트림 얻기
+      PrintWriter out = res.getWriter();
 
-		// 요청 전달데이터 얻기
-		String id = req.getParameter("id");
-		String pwd = req.getParameter("pwd");
+      // 요청 전달데이터 얻기
+      String id = req.getParameter("id");
+      String pwd = req.getParameter("pwd");
 
-		// jackson 라이브러리에서 제공하는 ObjectMapper 클래스 사용하기
-		ObjectMapper mapper = new ObjectMapper(); // JSON 문자열 만드는 API
+      // jackson 라이브러리에서 제공하는 ObjectMapper 클래스 사용하기
+      ObjectMapper mapper = new ObjectMapper(); // JSON 문자열 만드는 API
 
-		Map<String, Object> map = new HashMap<>();
+      Map<String, Object> map = new HashMap<>();
 
-		
-		HttpSession session = req.getSession();
-		// attribute가 있으면 제거함 
-		session.removeAttribute("loginedId");
+      
+      HttpSession session = req.getSession();
+      // attribute가 있으면 제거함 
+      session.removeAttribute("loginedId");
 
-		try {
-			service.login(id, pwd);
+      try {
+         service.login(id, pwd);
+         
+         System.out.println("===============> id : "+ id);
+         map.put("id", id);
 
-			map.put("status", 1);
-			map.put("msg", "로그인 되었습니다");
+         map.put("status", 0);
+         map.put("msg", "로그인 되었습니다");
 
-			// session에 loginedId 설정
-			session.setAttribute("loginedId", id);
-			
-			map.put("id", id);
-			
-			//서현 추가(로그인 시 닉네임도 저장)
-			CustomerDTO customerDTO = service.selectNickName(id);
-			String nickname = customerDTO.getNickname();
-			map.put("nickname", nickname);
-			//
+         // session에 loginedId 설정
+         session.setAttribute("loginedId", id);
+         
+         System.out.println("Session ID: " + session.getId());
+         
+         //서현 추가(로그인 시 닉네임도 저장)
+         CustomerDTO customerDTO = service.selectNickName(id);
+         String nickname = customerDTO.getNickname();
+         map.put("nickname", nickname);
+         //
 
-			System.out.println(session);
-		} catch (FindException e) {
-			e.printStackTrace();
-			map.put("status", 1);
-			map.put("msg", "다시 로그인해주세요.");
-		}
+         System.out.println(session);
+      } catch (FindException e) {
+         e.printStackTrace();
+         map.put("status", 1);
+         map.put("msg", "다시 로그인해주세요.");
+      }
 
-		String jsonStr = mapper.writeValueAsString(map);
-		out.print(jsonStr);
+      String jsonStr = mapper.writeValueAsString(map);
+      out.print(jsonStr);
+      
+      System.out.println(jsonStr);
 
-		return null;
+      return null;
 
-	} // execute
+   } // execute
 
 } // end class
